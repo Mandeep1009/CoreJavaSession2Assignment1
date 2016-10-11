7c1392de40b7196430e9f6afda2136443eab4f11
Mandeep1009 <yadavmandeep4@gmail.com>
2016-10-11 14:03:36 +0530

    Add files via upload

---
+++ PrintNameWithoutSemicolon.java
@@ -0,0 +1,11 @@
+//Write a program to print your name without using semicolon in the program.
+
+class PrintNameWithoutSemicolon
+{
+	public static void main(String[] args)
+	{
+		if (System.out.printf("Mandeep") != null) // printf() is a formating method to display format of a string
+		{
+		}
+	}
+}
